f164efe7944148d9d6ae7318183bdaa0da547333
kingHorey <Oreoluwaakinbo.oa@gmail.com>
2024-07-04 13:51:49 +0100

    change outpur format

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-
 """ import logging and annotation helpers """
 import re
 from typing import List
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
@@ -12,3 +12,21 @@ def filter_datum(fields: List[str], redaction: str, message: str,
         message = re.sub(f'{y}=.*?{separator}', f'{y}={redaction}{separator}',
                          message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ OUTPUT THE LOGGED MESSAGE """
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
+        return logging.Formatter(fmt=self.FORMAT).format(record)
