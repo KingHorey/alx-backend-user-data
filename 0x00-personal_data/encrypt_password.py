5343b7e2a48a9cc8976b45d12473daa58a8161dc
kingHorey <Oreoluwaakinbo.oa@gmail.com>
2024-07-04 18:03:43 +0100

    check hash password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,3 +8,9 @@ def hash_password(password: str) -> bytes:
     """ hash password """
     password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
     return password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ checks if a provided passwords is valid """
+    result = bcrypt.checkpw(password.encode("utf-8"), hashed_password)
+    return result
